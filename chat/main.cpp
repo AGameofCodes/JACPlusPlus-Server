2d29a60a03ed1db2d13989081e1ba9fd44f5876c
Peter Lorenz <lorenz.pet@hotmail.com>
2015-02-10 10:22:01 +0100

    first push

---
+++ chat/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: jester
+ *
+ * Created on February 10, 2015, 10:21 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
+
+
