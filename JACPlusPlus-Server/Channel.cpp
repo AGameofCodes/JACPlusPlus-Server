e1f195909e63b627517a5597ae8f157096bf6729
weaondara <wea_ondara@alpenblock.net>
2015-02-12 17:34:05 +0100

    added channel

---
+++ JACPlusPlus-Server/Channel.cpp
@@ -0,0 +1,69 @@
+/* 
+ * File:   Channel.cpp
+ * Author: alex
+ * 
+ * Created on 12. Februar 2015, 15:18
+ */
+
+#include "Channel.h"
+
+Channel::Channel(string name) : name(new string(name))
+{
+}
+
+Channel::Channel(const Channel& orig)
+{
+}
+
+Channel::~Channel()
+{
+  delete name;
+}
+
+//client management
+void Channel::addClient(Client *c)
+{
+  if(isMember(c))
+  {
+    return;
+  }
+  
+  clients.push_back(c);
+}
+void Channel::removeClient(Client *c)
+{
+  for(clientsIter it = clients.begin(); it != clients.end(); it++)
+  {
+    if(*it == c)
+    {
+      //todo remove it;
+      
+      return;
+    }
+  }
+}
+bool Channel::isMember(Client *c)
+{
+  for(clientsIter it = clients.begin(); it != clients.end(); it++)
+  {
+    if(*it == c)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+//getter
+
+list<Client*> Channel::getClients()
+{
+  //todo return a copy of this
+  return clients;
+}
+
+string Channel::getName()
+{
+  return string(*name);
+}
+
---
+++ JACPlusPlus-Server/Channel.h
@@ -0,0 +1,45 @@
+/* 
+ * File:   Channel.h
+ * Author: alex
+ *
+ * Created on 12. Februar 2015, 15:18
+ */
+
+#ifndef CHANNEL_H
+#define	CHANNEL_H
+
+#include <string>
+#include <list>
+
+using std::string;
+using std::list;
+
+class Client;
+
+class Channel
+{
+public:
+  Channel(string name);
+  virtual ~Channel();
+  
+  //client management
+  void addClient(Client *c);
+  void removeClient(Client *c);
+  bool isMember(Client *c);
+  
+  //getter
+  list<Client*> getClients();
+  string getName();
+private:
+  Channel(const Channel& orig);
+  
+  //vars
+  string *name;
+  list<Client*> clients;
+  
+  //iter
+  typedef list<Client*>::iterator clientsIter;
+};
+
+#endif	/* CHANNEL_H */
+
