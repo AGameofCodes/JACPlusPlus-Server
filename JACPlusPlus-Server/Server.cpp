7c3b156d85f28b7d93fc7e33946c9946a2992d4e
weaondara <wea_ondara@alpenblock.net>
2015-02-13 17:23:00 +0100

    close socket after connection; formatting

--- JACPlusPlus-Server/Server.cpp
+++ JACPlusPlus-Server/Server.cpp
@@ -20,153 +20,155 @@
 
 Server *Server::instance;
 
-Server::Server() 
+Server::Server()
 {
-    
+
 }
 
 Server::Server(const Server& orig)
 {
-  
+
 }
 
-Server::~Server() 
+Server::~Server()
 {
 }
 
 Server *Server::getInstance()
 {
-  if(Server::instance == NULL)
+  if (Server::instance == NULL)
   {
     Server::instance = new Server();
   }
   return Server::instance;
 }
 
-
-unsigned int Server::start() 
+unsigned int Server::start()
 {
   run();
 }
 
 //------------------------------------------------------------------------------
-unsigned int Server::stop() 
+
+unsigned int Server::stop()
 {
-    
+
 }
 
 
 //------------------------------------------------------------------------------
-unsigned int Server::run() 
-{
-    std::cout << "Creating socket." << std::endl;
-    Socket socket;
-    std::cout << "Binding to port." << std::endl;
-    socket.bind(5001);
-    std::cout << "Start listening." << std::endl;
-    socket.listen();
 
-    char buffer[256];
-    while(true)
+unsigned int Server::run()
+{
+  std::cout << "Creating socket." << std::endl;
+  Socket socket;
+  std::cout << "Binding to port." << std::endl;
+  socket.bind(5001);
+  std::cout << "Start listening." << std::endl;
+  socket.listen();
+
+  char buffer[256];
+  while (true)
+  {
+    try
     {
-        try
-        {
-            std::cout << "Accepting." << std::endl;
-            Socket *s = socket.accept();
-            std::cout << "Reading." << std::endl;
-            int bytes_read = s->read(buffer, 255);
-            std::cout << "Writing" << std::endl;
-            s->write("I got your message", 18);
-            delete s;
-        }
-        catch(IllegalStateException &e)
-        {
-            std::cerr << e.what() << std::endl;
-            return -1;
-        }
+      std::cout << "Accepting." << std::endl;
+      Socket *s = socket.accept();
+      std::cout << "Reading." << std::endl;
+      int bytes_read = s->read(buffer, 255);
+      std::cout << "Writing" << std::endl;
+      s->write("I got your message", 18);
+      s->close();
+      delete s;
     }
-    
-    /*int socketfd, newsocketfd;
-    int portnumber;
-    int response;
-    char buffer[256];
-    struct sockaddr_in server_addr, client_addr;
-    unsigned int clientaddrlen;
-    int status;
-    
-    cout << "Waiting for incoming connection..." << endl;
-    
-    //    int socket(int domain, int type, int protocol);
-    //    domain : The domain argument specifies a communication domain. In our case this value is AF_INET or AF_INET6 (the internet using ip4 or ip6) 
-    //    type : The type of socket. In our case it is SOCK_STREAM (tcp) 
-    //    protocol : The protocol to be used with the socket-type. In our case the right protocol is automatically choosen. 
-    //    return value : The socket system call returns a socket descriptor. If the socket call fails, it returns -1. 
-    socketfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socketfd < 0)
+    catch (IllegalStateException &e)
     {
-        cout << "Error: Opening Socket!" << endl;
-        return -1;
+      std::cerr << e.what() << std::endl;
+      return -1;
     }
+  }
+
+  /*int socketfd, newsocketfd;
+  int portnumber;
+  int response;
+  char buffer[256];
+  struct sockaddr_in server_addr, client_addr;
+  unsigned int clientaddrlen;
+  int status;
+    
+  cout << "Waiting for incoming connection..." << endl;
+    
+  //    int socket(int domain, int type, int protocol);
+  //    domain : The domain argument specifies a communication domain. In our case this value is AF_INET or AF_INET6 (the internet using ip4 or ip6) 
+  //    type : The type of socket. In our case it is SOCK_STREAM (tcp) 
+  //    protocol : The protocol to be used with the socket-type. In our case the right protocol is automatically choosen. 
+  //    return value : The socket system call returns a socket descriptor. If the socket call fails, it returns -1. 
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketfd < 0)
+  {
+      cout << "Error: Opening Socket!" << endl;
+      return -1;
+  }
      
-    //int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen); 
-    //sockfd : The socket descriptor the socket() call returns. 
-    //addr : The address we want to listen on (localhost). 
-    //addrlen : The lenght of this address. 
-    //Return value : Like all the other calls it also returns an integer. If it's '0' the call succeeded, if it's -1, we got an error that will be stored in errno as usual. 
-    //status = bind(socketfd, ai_a)
+  //int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen); 
+  //sockfd : The socket descriptor the socket() call returns. 
+  //addr : The address we want to listen on (localhost). 
+  //addrlen : The lenght of this address. 
+  //Return value : Like all the other calls it also returns an integer. If it's '0' the call succeeded, if it's -1, we got an error that will be stored in errno as usual. 
+  //status = bind(socketfd, ai_a)
     
-    //memset((char *) &serv_addr, sizeof(serv_addr));
-    portnumber = 5001;
+  //memset((char *) &serv_addr, sizeof(serv_addr));
+  portnumber = 5001;
     
   
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(portnumber);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(portnumber);
     
-    status = bind(socketfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
-    if(status < 0)
-    {
-        cout << "ERROR: Binding not possible!" << endl;
-        return -1;
-    }
+  status = bind(socketfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+  if(status < 0)
+  {
+      cout << "ERROR: Binding not possible!" << endl;
+      return -1;
+  }
     
     
-    //    int listen(int sockfd, int backlog); 
-    //    sockfd : The socket descriptor the socket() call returns. 
-    //    backlog : Our server can only handle 1 client at a time. What if more clients want to connect to your server at the same time? With backlog you can specify how many connections will be put in que. For example, if you set it to 5, and 7 connections to your server are made, 1 will fail, 1 will connect and the other 5 will be put "on hold". 
-    cout << "Listen()ing for connections..." << endl;
-    status  = listen(socketfd,5);
-    if(status < 0)
-    {
-        cout << "Error: Listener!" << endl;
-    }
+  //    int listen(int sockfd, int backlog); 
+  //    sockfd : The socket descriptor the socket() call returns. 
+  //    backlog : Our server can only handle 1 client at a time. What if more clients want to connect to your server at the same time? With backlog you can specify how many connections will be put in que. For example, if you set it to 5, and 7 connections to your server are made, 1 will fail, 1 will connect and the other 5 will be put "on hold". 
+  cout << "Listen()ing for connections..." << endl;
+  status  = listen(socketfd,5);
+  if(status < 0)
+  {
+      cout << "Error: Listener!" << endl;
+  }
     
-    clientaddrlen = sizeof(client_addr);
+  clientaddrlen = sizeof(client_addr);
     
-    while(1)
-    {
-        newsocketfd = accept(socketfd, (struct sockaddr *) &client_addr, &clientaddrlen);
-        if(newsocketfd < 0)
-        {
-            cout << "Error: Accept!" << endl;
-            return -1;       
-        }
+  while(1)
+  {
+      newsocketfd = accept(socketfd, (struct sockaddr *) &client_addr, &clientaddrlen);
+      if(newsocketfd < 0)
+      {
+          cout << "Error: Accept!" << endl;
+          return -1;       
+      }
 
-        status = read(newsocketfd, buffer, 255);
-        if(status < 0)
-        {
-            cout << "Error: Reading from Socket!" << endl;
-            return -1;
-        }
+      status = read(newsocketfd, buffer, 255);
+      if(status < 0)
+      {
+          cout << "Error: Reading from Socket!" << endl;
+          return -1;
+      }
 
-        status = write(newsocketfd, "I got your message", 18);
-        if(status < 0)
-        {
-            cout << "Error: Writing to Socket!" << endl;
-            return -1;
-        }
-    }
-    */
+      status = write(newsocketfd, "I got your message", 18);
+      if(status < 0)
+      {
+          cout << "Error: Writing to Socket!" << endl;
+          return -1;
+      }
+  }
+   */
 }
 
 
