0cc48d54c2bf6ebb77dd9489f136802e7d86106e
Peter Lorenz <lorenz.pet@hotmail.com>
2015-02-18 17:51:06 +0100

    pthread header

---
+++ JACPlusPlus-Server/ConnectionHandling.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   ConnectionHandling.h
+ * Author: jester
+ *
+ * Created on February 18, 2015, 5:47 PM
+ */
+
+#ifndef CONNECTIONHANDLING_H
+#define	CONNECTIONHANDLING_H
+
+class ConnectionHandling {
+public:
+    ConnectionHandling();
+    ConnectionHandling(const ConnectionHandling& orig);
+    virtual ~ConnectionHandling();
+private:
+
+};
+
+#endif	/* CONNECTIONHANDLING_H */
+
