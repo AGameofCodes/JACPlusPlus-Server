24be5e76bf6bf094c1260f649faa21b2cc6d254b
Peter Lorenz <lorenz.pet@hotmail.com>
2015-02-18 17:48:18 +0100

    pthread class

---
+++ JACPlusPlus-Server/ConnectionHandling.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   ConnectionHandling.cpp
+ * Author: jester
+ * 
+ * Created on February 18, 2015, 5:48 PM
+ */
+
+#include "ConnectionHandling.h"
+
+ConnectionHandling::ConnectionHandling() {
+}
+
+ConnectionHandling::ConnectionHandling(const ConnectionHandling& orig) {
+}
+
+ConnectionHandling::~ConnectionHandling() {
+}
+
