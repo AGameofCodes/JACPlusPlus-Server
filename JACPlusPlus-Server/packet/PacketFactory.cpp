a81d0d0535443fc8653ea93c29dd929e9bd722ef
weaondara <wea_ondara@alpenblock.net>
2015-02-19 02:45:06 +0100

    added packet factory

---
+++ JACPlusPlus-Server/packet/PacketFactory.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   PacketFactory.cpp
+ * Author: alex
+ * 
+ * Created on 19. Februar 2015, 02:38
+ */
+
+#include "PacketFactory.h"
+
+PacketFactory *PacketFactory::instance;
+
+PacketFactory *PacketFactory::getInstance()
+{
+  if (PacketFactory::instance == nullptr)
+  {
+    instance = new PacketFactory();
+  }
+  return PacketFactory::instance;
+}
+
+PacketFactory::PacketFactory()
+{
+}
+
+Packet *PacketFactory::create()
+{
+  return nullptr;
+}
+
---
+++ JACPlusPlus-Server/packet/PacketFactory.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   PacketFactory.h
+ * Author: alex
+ *
+ * Created on 19. Februar 2015, 02:38
+ */
+
+#ifndef PACKETFACTORY_H
+#define	PACKETFACTORY_H
+
+#include "Packet.h"
+
+class PacketFactory
+{
+public:
+  static PacketFactory *getInstance();
+  
+  PacketFactory(const PacketFactory& orig) = delete;
+  virtual ~PacketFactory() = delete;
+  
+  Packet *create();
+private:
+  static PacketFactory *instance;
+  
+  PacketFactory();
+};
+
+#endif	/* PACKETFACTORY_H */
+
